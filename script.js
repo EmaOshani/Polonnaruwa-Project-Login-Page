f9dc6f47e6c6b4d6968c05858ecd686c3272117c
EmaOshani <imashasamarasekara99@gmail.com>
2024-02-12 19:24:39 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+// script.js
+//confirem & send registration details
+
+document.addEventListener("DOMContentLoaded", function() {
+    var signUpLink = document.getElementById("signup-link");
+
+    signUpLink.addEventListener("click", function() {
+        window.location.href = "signup.html";
+    });
+});
